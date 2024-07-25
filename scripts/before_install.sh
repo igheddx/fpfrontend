0b684173a6eb68b369fbd9d93b49a1761cf10b62
Dominic Ighedosa <ighedosa.me@gmail.com>
2024-07-24 22:03:52 -0500

    update

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -9,7 +9,7 @@
 #sudo apt-get install curl
 #sudo curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
 #sudo apt-get install nodejs -y
-sudo apt-get install npm -y
+#sudo apt-get install npm -y
 #sudo apt install nginx -y
 #sudo ufw allow 'Nginx HTTP'
 
